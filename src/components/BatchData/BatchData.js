db00aafa2be6e57001792d6849949491e608db3b
purplevaruntw <varun.kedia@thoughtworks.com>
2022-03-14 17:53:18 +0530

    added logic to count requirements

--- src/components/BatchData/BatchData.js
+++ src/components/BatchData/BatchData.js
@@ -1,30 +1,54 @@
-import React, { useEffect } from "react";
-import "./BatchData.css"
+import React from "react";
+import "./BatchData.css";
 const BatchData = ({ data }) => {
-	const displayProgress = (progress) =>{
-		return <h1>{progress.completionStatus}</h1>
-		if (progress.projectName==="trainingCamp"){
-
+	const getFullName = (progress) => {
+		const prefix = progress.projectName;
+		var fullName = "";
+		if (prefix === "trainingCamp") {
+			fullName = "Training Camp";
+		} else {
+			fullName =
+				prefix + " - Requirement " + (progress.requirementNumber + 1);
 		}
-		else {
-
-		}
-	}
+		return fullName;
+	};
+	const getCount = (matcherString) => {
+		var count = 0;
+		data.forEach((val) => {
+			const fullName = getFullName(
+				val.progressResponse[val.progressResponse.length - 1]
+			);
+			if (fullName === matcherString) count++;
+		});
+		return count;
+	};
+	const displayProgress = (progress) => {
+		const fullName = getFullName(progress);
+		const count = getCount(fullName);
+		return (
+			<h3>
+				{fullName} and {count}{" "}
+			</h3>
+		);
+	};
 	return (
-		<div >
+		<div>
 			<h1>Progress Status</h1>
-			<hr/>
-		<div className="progress-status-container">
-
-		{data.map((val, idx) => {
-				return (
-					<div key={idx} className="progress-status">
-						{displayProgress(val.progressResponse[val.progressResponse.length - 1])}
-
-					</div>
-				);
-			})}
-		</div>
+			<hr />
+			<div className="progress-status-container">
+				{data.map((val, idx) => {
+					return (
+						<div key={idx} className="progress-status">
+							{displayProgress(
+								val.progressResponse[
+									val.progressResponse.length - 1
+								],
+								val
+							)}
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
