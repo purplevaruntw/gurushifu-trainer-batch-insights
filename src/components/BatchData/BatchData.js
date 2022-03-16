8d9ea7d1ced03e5724ab6f926bab6fbe0352cb9a
Saloni Gupta <saloni.gupta@thoughtworks.com>
2022-03-16 15:57:31 +0530

    Added logic to display count of trainees against eacg requirement

--- src/components/BatchData/BatchData.js
+++ src/components/BatchData/BatchData.js
@@ -1,37 +1,57 @@
 import React from "react";
 import TableRow from "../TableRow/TableRow";
 import "./BatchData.css";
+
 const BatchData = ({ data }) => {
-	const getFullName = (progress) => {
-		const prefix = progress.projectName;
-		var fullName = "";
-		if (prefix === "trainingCamp") {
-			fullName = "Training Camp";
-		} else {
-			fullName =
-				prefix + " - Requirement " + (progress.requirementNumber + 1);
-		}
-		return fullName;
-	};
-	const getCount = (matcherString) => {
-		var count = 0;
-		data.forEach((val) => {
-			const fullName = getFullName(
-				val.progressResponse[val.progressResponse.length - 1]
-			);
-			if (fullName === matcherString) count++;
-		});
-		return count;
-	};
+	const getProjectName = {"rectangle":"Rectangle", "trainingCamp":"Training Camp", "make-me-oo":"Make Me OO"};
+	const getRequirement = {"Rectangle":3,"Make Me OO":4};
 	var counter = new Map();
+	const batchProjectName = "rectangle";
+	
+	const getCount = (fullName) => {
+		counter.set(fullName,counter.get(fullName)+1);
+	}
+	
+	const setInitialCount = (projectName) => {
+		const prefix = getProjectName[projectName];
+		var fullName = "";
+		fullName = "Training Camp";
+		counter.set(fullName,0);
+		
+		for (let i=0;i<getRequirement[prefix];i++)
+		{
+			fullName = prefix + " - Requirement " + (i + 1);
+			counter.set(fullName,0);
+		}
+		fullName = prefix + "- Completed"
+		counter.set(fullName,0);
+	}
+	
+	setInitialCount(batchProjectName);
+	
+	const getFullName = (progress) => {
+		const prefix = getProjectName[progress.projectName];
+		var fullName = "";
+		if (prefix==="Training Camp"){
+			fullName = "Training Camp"
+		}
+		else if (prefix!=="Training Camp" && progress.completionStatus === "Completed"){
+			fullName = prefix + "- Completed"
+		}
+		else{
+			fullName = prefix + " - Requirement " + (progress.requirementNumber + 1)
+		}
+		return fullName
+	}
+	
 	const displayProgress = (progress) => {
 		const fullName = getFullName(progress);
 		const count = getCount(fullName);
-		if (counter[fullName]) return;
-
-		counter.set(fullName, count);
+		console.log(count);
+		//counter.set(fullName, count);
 		counter = new Map([...counter].sort());
 		console.log("counter obj = ", counter);
+	
 	};
 	return (
 		<div>
@@ -47,6 +67,7 @@ const BatchData = ({ data }) => {
 								],
 								val
 							)}
+							{}
 						</div>
 					);
 				})}
