427e22f9dee8a1c078351d0fb1b14efc3e8555e9
purplevaruntw <varun.kedia@thoughtworks.com>
2022-03-16 17:37:59 +0530

    Added counter logic in the lambda function

--- src/App.js
+++ src/App.js
@@ -2,28 +2,33 @@ import "./App.css";
 import Button from "./components/Button/Button";
 import config from "./api/config";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BatchData from "./components/BatchData/BatchData";
 
 function App() {
 	const [data, setData] = useState(null);
-	const [showButton, setShowButton] = useState(true)
+	const [showButton, setShowButton] = useState(true);
 	const getData = async () => {
 		const url = config.API.batchInsights;
 		const { data } = await axios.get(url);
 		setData(data);
-		console.log(data);
 	};
 	const showBatchInsights = () => {
 		getData();
 		setShowButton(false);
-		
 	};
-
+	useEffect(() => {
+		document.title = "Batch Insights";
+	});
 	return (
 		<div className="App">
-			{showButton && <Button value="Batch Insights" onClickFunction={showBatchInsights} />}
-			{data ? <BatchData data={data.body.Items} /> : ""}
+			{showButton && (
+				<Button
+					value="Batch Insights"
+					onClickFunction={showBatchInsights}
+				/>
+			)}
+			{data ? <BatchData data={data.body} /> : ""}
 		</div>
 	);
 }
--- src/components/BatchData/BatchData.js
+++ src/components/BatchData/BatchData.js
@@ -1,75 +1,14 @@
 import React from "react";
 import TableRow from "../TableRow/TableRow";
 import "./BatchData.css";
-
 const BatchData = ({ data }) => {
-	const getProjectName = {"rectangle":"Rectangle", "trainingCamp":"Training Camp", "make-me-oo":"Make Me OO"};
-	const getRequirement = {"Rectangle":3,"Make Me OO":4};
-	var counter = new Map();
-	const batchProjectName = "rectangle";
-	
-	const getCount = (fullName) => {
-		counter.set(fullName,counter.get(fullName)+1);
-	}
-	
-	const setInitialCount = (projectName) => {
-		const prefix = getProjectName[projectName];
-		var fullName = "";
-		fullName = "Training Camp";
-		counter.set(fullName,0);
-		
-		for (let i=0;i<getRequirement[prefix];i++)
-		{
-			fullName = prefix + " - Requirement " + (i + 1);
-			counter.set(fullName,0);
-		}
-		fullName = prefix + "- Completed"
-		counter.set(fullName,0);
-	}
-	
-	setInitialCount(batchProjectName);
-	
-	const getFullName = (progress) => {
-		const prefix = getProjectName[progress.projectName];
-		var fullName = "";
-		if (prefix==="Training Camp"){
-			fullName = "Training Camp"
-		}
-		else if (prefix!=="Training Camp" && progress.completionStatus === "Completed"){
-			fullName = prefix + "- Completed"
-		}
-		else{
-			fullName = prefix + " - Requirement " + (progress.requirementNumber + 1)
-		}
-		return fullName
-	}
-	
-	const displayProgress = (progress) => {
-		const fullName = getFullName(progress);
-		const count = getCount(fullName);
-		console.log(count);
-		//counter.set(fullName, count);
-		counter = new Map([...counter].sort());
-		console.log("counter obj = ", counter);
-	
-	};
 	return (
 		<div>
 			<h1>Progress Status</h1>
 			<hr />
-			<div className="progress-status-container">
-				{data.map((val, idx) => {
-					return (
-						<div key={idx} className="progress-status">
-							{displayProgress(
-								val.progressResponse[
-									val.progressResponse.length - 1
-								],
-								val
-							)}
-							{}
-						</div>
-					);
+			<div className="table">
+				{Object.keys(data).map((val, key) => {
+					return <TableRow key={key} name={val} count={data[val]} />;
 				})}
 			</div>
 		</div>
